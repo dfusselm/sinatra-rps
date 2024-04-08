7ab38a96419c42c5a48456b3edc69b4d8b1f227d
dfusselm <165109847+dfusselm@users.noreply.github.com>
2024-04-08 16:00:19 +0000

    rps

--- app.rb
+++ app.rb
@@ -7,3 +7,54 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  @player_move = "rock"
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample  
+
+if @comp_move == "rock"
+  @outcome = "tied"
+elsif @comp_move == "paper"
+  @outcome = "lost"
+elsif @comp_move == "scissors"
+  @outcome = "won"
+end
+
+  erb(:rps)
+
+end
+
+get("/paper") do
+  @player_move = "paper"
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample  
+
+if @comp_move == "rock"
+  @outcome = "won"
+elsif @comp_move == "paper"
+  @outcome = "tied"
+elsif @comp_move == "scissors"
+  @outcome = "lost"
+end
+
+  erb(:rps)
+
+end
+
+get("/scissors") do
+  @player_move = "scissors"
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample  
+
+if @comp_move == "rock"
+  @outcome = "lost"
+elsif @comp_move == "paper"
+  @outcome = "won"
+elsif @comp_move == "scissors"
+  @outcome = "tied"
+end
+
+  erb(:rps)
+
+end
